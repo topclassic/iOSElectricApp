e9f6033cb833ad8eb6e7d72f8bffdcdc932e876a
Chotipat Top <topz.classic@gmail.com>
2016-05-21 21:45:26 +0700

    Create Bar Chart
    import chart

---
+++ iOSElectricApp/BarChartController.swift
@@ -0,0 +1,67 @@
+//
+//  BarChartViewController.swift
+//  iOSChartsDemo
+//
+//  Created by Joyce Echessa on 6/12/15.
+//  Copyright (c) 2015 Appcoda. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class BarChartController: UIViewController, ChartViewDelegate {
+    
+    @IBOutlet weak var barChartView: BarChartView!
+    
+    var months: [String]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        barChartView.delegate = self
+        
+        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
+        
+        setChart(months, values: unitsSold)
+    }
+    
+    func setChart(dataPoints: [String], values: [Double]) {
+        barChartView.noDataText = "You need to provide data for the chart."
+        
+        var dataEntries: [BarChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = BarChartDataEntry(value: values[i], xIndex: i)
+            dataEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = BarChartDataSet(yVals: dataEntries, label: "Units Sold")
+        let chartData = BarChartData(xVals: months, dataSet: chartDataSet)
+        barChartView.data = chartData
+        
+        barChartView.descriptionText = ""
+        
+        chartDataSet.colors = [UIColor(red: 230/255, green: 126/255, blue: 34/255, alpha: 1)]
+        //        chartDataSet.colors = ChartColorTemplates.colorful()
+        
+        barChartView.xAxis.labelPosition = .Bottom
+        
+        //        barChartView.backgroundColor = UIColor(red: 189/255, green: 195/255, blue: 199/255, alpha: 1)
+        
+        //        barChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
+        barChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0, easingOption: .EaseInBounce)
+        
+        let ll = ChartLimitLine(limit: 10.0, label: "Target")
+        barChartView.rightAxis.addLimitLine(ll)
+        
+    }
+    
+    func chartValueSelected(chartView: ChartViewBase, entry: ChartDataEntry, dataSetIndex: Int, highlight: ChartHighlight) {
+        //    println("\(entry.value) in \(months[entry.xIndex])")
+    }
+    
+    
+}
+
