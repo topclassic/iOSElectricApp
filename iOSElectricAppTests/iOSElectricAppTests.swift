d691fbf0a13cf3c07285e5fce835a92ac943b256
Chotipat Top <topz.classic@gmail.com>
2016-05-21 18:57:06 +0700

    Initial Commit

---
+++ iOSElectricAppTests/iOSElectricAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOSElectricAppTests.swift
+//  iOSElectricAppTests
+//
+//  Created by Chotipat on 5/21/2559 BE.
+//  Copyright © 2559 Chotipat. All rights reserved.
+//
+
+import XCTest
+@testable import iOSElectricApp
+
+class iOSElectricAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
